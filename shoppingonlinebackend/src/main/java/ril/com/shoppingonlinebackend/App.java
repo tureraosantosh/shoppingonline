23a09fb7c0042cbb1b7361afc482687bfff9d07a
santosh turerao <tureraosantosh@gmail.com>
2017-09-19 12:33:58 +0530

    part 01 is completed successfully

---
+++ shoppingonlinebackend/src/main/java/ril/com/shoppingonlinebackend/App.java
@@ -0,0 +1,13 @@
+package ril.com.shoppingonlinebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
